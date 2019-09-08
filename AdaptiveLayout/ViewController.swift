c1cdae9bab0e6d4d6a3ecdb83e57f9c8bccc583a
Mazen Kasser <mazen.kasser@anz.com>
2019-09-08 18:06:36 +1200

    Initial Commit

---
+++ AdaptiveLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveLayout
+//
+//  Created by Mazen on 8/09/19.
+//  Copyright © 2019 iProgram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
